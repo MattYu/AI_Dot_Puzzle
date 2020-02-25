25cd50c44519b932a82566cae5ad4e1f540380d3
MattYu <mingtyut@gmail.com>
2020-02-24 21:11:44 -0500

    shrinked main file

--- bfs.py
+++ bfs.py
@@ -10,6 +10,7 @@ class BFS(Matrix):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.maxExploratoryDepth = kwargs.pop('maxExploratoryDepth', 0)
+        self.type = "bfs"
 
     def getMinHWithExploratorySearch(self, currentMatrix, maxDepth):
         stack = deque()
@@ -92,49 +93,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         bfs.maxExploratoryDepth = int(sys.argv[1])
 
-    input_file = open("input.txt", "r")
-    line_nbr = 0
-
-    for line in input_file:
-        solution = open(str(line_nbr)+"_bfs_solution.txt", "w")
-        search = open(str(line_nbr)+"_bfs_search.txt", "w")
-        bfs.seen = {}
-        
-        data = line.split()
-        size = int(data[0])
-        max_d = int(data[1])
-        max_l = int(data[2])
-        puzzle = data[3]
-
-        graph = n.createGraph(puzzle, size)
-
-        bfs.size = size
-        bfs.maxDepth = max_d
-        bfs.solution = str(graph)
-
-        node = Node()
-        node.depth = 1
-        node.move = '0 '
-        node.matrix = graph
-        
-        bfs.startNode = node
-        bfs.expectedResult = str(n.createGraph("0"*(size*size), size))
-        ans = bfs.run(search)
-
-        res = ans
-        path = []
-        if ans == None:
-            solution.write("No solution")
-        while ans != None:
-            path.append(ans)
-            ans = ans.parentNode
-            
-        for sol in reversed(path):
-            solution.write(sol.move + ' ')
-            solution.write(''.join([''.join(element) for element in sol.matrix])+'\n')
-            
-        line_nbr += 1
-            
-    input_file.close()
-    solution.close()
-    search.close()
+    bfs.writeOutput()
--- dfs.py
+++ dfs.py
@@ -7,6 +7,7 @@ class dfs(Matrix):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.type = "dfs"
 
     def run(self, path):
 
@@ -45,51 +46,5 @@ class dfs(Matrix):
                 stack.extend(priority)
 
 if __name__ == "__main__":
-    n = Node()
     dfs = dfs()
-    input_file = open("input.txt", "r")
-    line_nbr = 0
-
-    for line in input_file:
-        solution = open(str(line_nbr)+"_dfs_solution.txt", "w")
-        search = open(str(line_nbr)+"_dfs_search.txt", "w")
-        dfs.seen = {}
-        
-        data = line.split()
-        size = int(data[0])
-        max_d = int(data[1])
-        max_l = int(data[2])
-        puzzle = data[3]
-
-        graph = n.createGraph(puzzle, size)
-
-        dfs.size = size
-        dfs.maxDepth = max_d
-        dfs.solution = str(graph)
-
-        node = Node()
-        node.depth = 1
-        node.move = '0 '
-        node.matrix = graph
-        
-        dfs.startNode = node
-        dfs.expectedResult = str(n.createGraph("0"*(size*size), size))
-        ans = dfs.run(search)
-
-        res = ans
-        path = []
-        if ans == None:
-            solution.write("No solution")
-        while ans != None:
-            path.append(ans)
-            ans = ans.parentNode
-            
-        for sol in reversed(path):
-            solution.write(sol.move + ' ')
-            solution.write(''.join([''.join(element) for element in sol.matrix])+'\n')
-            
-        line_nbr += 1
-            
-    input_file.close()
-    solution.close()
-    search.close()
+    dfs.writeOutput()
